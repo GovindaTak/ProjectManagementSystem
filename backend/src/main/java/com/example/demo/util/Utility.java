a4dc37c430dc0944726d6d7d07551f675ec348f5
Govinda-Tak <govinda.tak@atmecs.com>
2024-07-19 13:04:37 +0530

    update in util functions

--- backend/src/main/java/com/example/demo/util/Utility.java
+++ backend/src/main/java/com/example/demo/util/Utility.java
@@ -13,20 +13,35 @@ public final class Utility {
 	@Autowired
 	private static Cloudinary cloud;
 
-	public static String uploadImage(MultipartFile image) throws IOException
+	public static String uploadImage(MultipartFile image) 
 	{
+		try {
 	Map uploadResult=	cloud.uploader().upload(image.getBytes(), ObjectUtils.asMap("folder","Project_Management_System"));
 	return uploadResult.get("url").toString();
+		}
+		catch(Exception ex)
+		{
+			throw new RuntimeException(ex.getMessage());
+		}
 	}
 	
 	public static void removeImage(String url)
 	{
+		try {
 		String[] urlParts =url.split("/");
 		
 		String publicIdWithExtension=urlParts[urlParts.length-1];
 		String publicId=publicIdWithExtension.split("//.")[0];
 		
-		Map result=cloud.uploader().destroy(publicId, ObjectUtils.asMap("folder","Project_Management_System");
+		Map result=cloud.uploader().destroy(publicId, ObjectUtils.asMap("folder","Project_Management_System"));
+		if("ok".equals(result.get("result")))
+		{ return ;}
+		else throw new RuntimeException("something wrong in image removal from cloudinary !!!");
+		}
+		catch(Exception ex)
+		{
+			throw new RuntimeException(ex.getMessage());
+		}
 	}
 	
 }
